a8d90bd5ee0654426b869211155914a5738e0c21
ChandlerNgo <chandlerngo00@gmail.com>
2022-05-12 23:30:31 -0500

    displayed image on html

--- HangmanApp/views.py
+++ HangmanApp/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    import random
+    wrong = 0
+    words = ("enigma","pneumonia","keyboard", "burger", "sheesh", "broken", "brazil", "vietnam")
+    guessword = random.choice(words)
+    guessword = guessword.upper()
+    wordlength = len(guessword)
+    fillword = ""
+    guesslist = []
+    alphabet_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    response = {
+        "gamestate": f"static/state{wrong}.png/"
+    }
+    return render(request,"index.html",response)
